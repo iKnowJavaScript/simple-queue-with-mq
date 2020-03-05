dff2ae39702e9aa3f8ad070162a61277f1635168
Omolayo Victor <vomolayo@seamfix.com>
2020-03-05 17:54:22 +0100

    Implement the consumer

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const amqp = require("amqplib/callback_api");
+let queue = "sample_queue";
 
-amqp.connect("amqp://localhost", function(error, connection) {
-  if (error) {
-    throw error;
+// Producer
+amqp.connect("amqp://localhost", function(connectionError, connection) {
+  if (connectionError) {
+    throw connectionError;
   }
 
-  connection.createChannel(function(connectionError, channel) {
-    if (connectionError) {
-      throw connectionError;
+  connection.createChannel(function(channelError, channel) {
+    if (channelError) {
+      throw channelError;
     }
 
-    let queue = "sample_queue";
     let message = "Hello world";
 
     channel.assertQueue(queue, { durable: true });
@@ -19,8 +20,35 @@ amqp.connect("amqp://localhost", function(error, connection) {
     console.log("Sent '%s'", message);
   });
 
-  setTimeout(function() {
+  setTimeout(() => {
     connection.close();
     process.exit(0);
   }, 500);
+});
+
+// Consumer
+amqp.connect("amqp://localhost", function(connectionError, connection) {
+  if (connectionError) {
+    throw connectionError;
+  }
+
+  connection.createChannel(function(channelError, channel) {
+    if (channelError) {
+      throw channelError;
+    }
+
+    // its possible for consumer to start before producer
+    channel.assertQueue(queue, { durable: true });
+
+    channel.prefetch(1);
+
+    console.log("Waiting for messages in %s", queue);
+    channel.consume(queue, function(message) {
+      console.log("Received '%s'", msg.content.toString());
+
+      setTimeout(() => {
+        channel.ack(message);
+      }, 1000);
+    });
+  });
 });
